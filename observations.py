d989351018b57d8be6032669b4b54e2962b276ac
DanielAndreasen <daniel.andreasen@astro.up.pt>
2017-02-01 15:10:16 +0000

    Small updates to documentation and code

--- observations.py
+++ observations.py
@@ -9,36 +9,50 @@ from astropy.io import fits
 import matplotlib.pyplot as plt
 
 
-def local_norm(obs_fname, r, snr, method='linear', plot=False):
+def local_norm(obs_fname, r, snr=None, method='linear', plot=False):
     '''Local Normalisation function. Make a linear fit from the maximum points
     of each segment.
+
     Input
     -----
-    obs_fname : observations file
-    r : range of the interval
+    obs_fname : str
+      Observations file
+    r : list
+      range of the interval
+    method : str
+      'scalar' : Normalize with median value
+      'linear' : Normalize with a linear fit
+    plot : bool
+      Show plot
 
     Output
     ------
-    new_flux : normalized flux
+    wave : ndarray
+      Wavelength
+    new_flux : ndarray
+      Normalized flux
+    delta_l : float
+      Wavelength step
     '''
 
     # Define the area of Normalization
     start_norm = r[0]
     end_norm = r[1]
-    #Transform SNR to noise
+    # Transform SNR to noise
     if snr is None:
         noise = 0.0
     else:
         snr = float(snr)
         noise = 1.0/(snr)
-    #Read observations
+
+    # Read observations
     wave_obs, flux_obs, delta_l = read_observations(obs_fname, start_norm, end_norm)
 
     # Divide in 2 and find the maximum points
     y = np.array_split(flux_obs, 2)
     x = np.array_split(wave_obs, 2)
-    index_max1 = np.sort(np.argsort(y[0])[-8:])  # this can be done better
-    index_max2 = np.sort(np.argsort(y[1])[-8:])  # this can be done better
+    index_max1 = np.sort(np.argsort(y[0])[-8:])
+    index_max2 = np.sort(np.argsort(y[1])[-8:])
     f_max1 = y[0][index_max1]
     f_max2 = y[1][index_max2]
 
@@ -73,8 +87,6 @@ def local_norm(obs_fname, r, snr, method='linear', plot=False):
             new_flux =  new_flux + (0.0*noise)
 
     # Exclude some continuum points which differ 0.5% from continuum level
-    #wave_obs = wave_obs[np.where((1.0-new_flux)/new_flux > 0.005)]
-    #new_flux = new_flux[np.where((1.0-new_flux)/new_flux > 0.005)]
     wave = wave_obs[np.where((wave_obs >= float(r[0])) & (wave_obs <= float(r[1])))]
     new_flux = new_flux[np.where((wave_obs >= float(r[0])) & (wave_obs <= float(r[1])))]
 
@@ -84,7 +96,7 @@ def local_norm(obs_fname, r, snr, method='linear', plot=False):
         y = [np.median(f_max), np.median(f_max)]
         plt.plot(x, y)
         plt.plot(w_max, f_max, 'o')
-        plt.show()
+        plt.show()  # Are you sure you want this here?
 
         y = [1.0, 1.0]
         plt.plot(x, y)
@@ -94,25 +106,30 @@ def local_norm(obs_fname, r, snr, method='linear', plot=False):
 
 
 def read_observations(fname, start_synth, end_synth):
-    """Read observed spectrum of different types and return wavelength and flux.
+    '''Read observed spectrum of different types and return wavelength and flux.
     Input
     -----
-    fname : filename of the spectrum. Currently only fits and text files accepted.
-    start_synth : starting wavelength where the observed spectrum is cut
-    end_synth : ending wavelength where the observed spectrum is cut
+    fname : str
+      filename of the spectrum. Currently only fits and text files accepted.
+    start_synth : float
+      starting wavelength where the observed spectrum is cut
+    end_synth : float
+      ending wavelength where the observed spectrum is cut
 
     Output
     -----
-    wavelength_obs : observed wavelength
-    flux_obs : observed flux
-    """
+    wavelength_obs : ndarray
+      observed wavelength
+    flux_obs : ndarray
+      observed flux
+    '''
     # These are the approved formats
     extension = ('.dat', '.txt', '.spec', '.fits')
     if fname.endswith(extension):
         if (fname[-4:] == '.dat') or (fname[-4:] == '.txt'):
             with open(fname, 'r') as f:
                 lines = (line for line in f if not line[0].isalpha())  # skip header
-                wave, flux = np.loadtxt(lines, unpack=True, usecols=(0, 1))
+                wave, flux = np.loadtxt(lines, unpack=True, usecols=(0, 1))  # Not sure this is right. Maybe np.array(lines)
 
         elif fname[-5:] == '.fits':
             hdulist = fits.open(fname)
@@ -121,12 +138,11 @@ def read_observations(fname, start_synth, end_synth):
             flux = hdulist[0].data  # flux data in the primary
             flux = np.array(flux, dtype=np.float64)
             start_wave = header['CRVAL1']  # initial wavelenght
-            # step = header['CD1_1'] #step in wavelenght
             step = header['CDELT1']  # increment per pixel
             w0, dw, n = start_wave, step, len(flux)
             w = start_wave + step * n
             wave = np.linspace(w0, w, n, endpoint=False)
-        # T hese types are produced by MOOGme (fits format).
+        # These types are produced by FASMA (fits format).
         elif fname[-5:] == '.spec':
             hdulist = fits.open(fname)
             x = hdulist[1].data
@@ -136,7 +152,6 @@ def read_observations(fname, start_synth, end_synth):
         delta_l = wave[1] - wave[0]
         wavelength_obs = wave[np.where((wave >= float(start_synth)) & (wave <= float(end_synth)))]
         flux_obs = flux[np.where((wave >= float(start_synth)) & (wave <= float(end_synth)))]
-
     else:
         print('Spectrum is not in acceptable format. Convert to ascii of fits.')
         wavelength_obs, flux_obs, delta_l = (None, None, None)
@@ -144,7 +159,7 @@ def read_observations(fname, start_synth, end_synth):
 
 
 def read_obs_intervals(obs_fname, r, snr=100, method='linear'):
-    """Read only the spectral chunks from the observed spectrum
+    '''Read only the spectral chunks from the observed spectrum
     This function does the same as read_observations but for the whole linelist.
     Input
     -----
@@ -156,7 +171,7 @@ def read_obs_intervals(obs_fname, r, snr=100, method='linear'):
     -----
     wavelength_obs : observed wavelength
     flux_obs : observed flux
-    """
+    '''
 
     # N : number of intervals
     N = len(r)
@@ -177,23 +192,29 @@ def read_obs_intervals(obs_fname, r, snr=100, method='linear'):
     return x_obs, y_obs, delta_l
 
 
-def plot(x_obs, y_obs, x, y, res=False):
-    """Function to plot synthetic spectrum.
+def plot(x_obs=None, y_obs=None, x=None, y=None, res=False):
+    '''Function to plot synthetic spectrum.
     Input
     -----
-    x_obs : observed wavelength
-    y_obs : observed flux
-    x : synthetic wavelength
-    y : synthetic flux
+    x_obs : ndarray
+      observed wavelength
+    y_obs : ndarray
+      observed flux
+    x : ndarray
+      synthetic wavelength
+    y : ndarray
+      synthetic flux
+    res : bool
+      If True, then calculate the difference in flux
 
     Output
     ------
     plots
-    """
+    '''
 
-    # if nothing exists, pass
+    # if nothing exists, exit
     if (x_obs is None) and (x is None):
-        pass
+        return
     # if there is not observed spectrum, plot only synthetic (case 1, 3)
     if x_obs is None:
         plt.plot(x, y, label='synthetic')
@@ -201,7 +222,7 @@ def plot(x_obs, y_obs, x, y, res=False):
             sl = InterpolatedUnivariateSpline(x, y, k=1)
             ymodel = sl(x_obs)
             plt.plot(x_obs, (y_obs-ymodel)*10, label='residuals')
-        plt.legend()
+        plt.legend(loc='best', frameon=False)
         plt.show()
     # if both exist
     else:
@@ -212,13 +233,13 @@ def plot(x_obs, y_obs, x, y, res=False):
             ymodel = sl(x_obs)
             plt.plot(x_obs, (y_obs-ymodel)*10, label='residuals')
 
-        plt.legend()
+        plt.legend(loc='best', frameon=False)
         plt.show()
     return
 
 
 def snr(fname, plot=False):
-    """Calculate SNR using intervals depending on giraffe mode.
+    '''Calculate SNR using intervals depending on giraffe mode.
     Input
     ----
     fname : str
@@ -230,7 +251,7 @@ def snr(fname, plot=False):
     -----
     snr : float
       snr value averaged from the continuum intervals
-    """
+    '''
     from PyAstronomy import pyasl
     def sub_snr(interval):
         '''Measure the SNR on a small interval
@@ -255,9 +276,9 @@ def snr(fname, plot=False):
             return 0
 
 
-    intervals = [[5744, 5746], [6048, 6052], [6068, 6076], [6682, 6686],
-                 [6649, 6652], [6614, 6616], [5374.5, 5376.5], [5438.5, 5440],
-                 [5449.5, 5051], [5458, 5459.25], [5498.3, 5500], [5541.5, 5542.5]]
+    intervals = [[5744, 5746],   [6048, 6052],    [6068, 6076],     [6682, 6686],
+                 [6649, 6652],   [6614, 6616],    [5374.5, 5376.5], [5438.5, 5440],
+                 [5449.5, 5051], [5458, 5459.25], [5498.3, 5500],   [5541.5, 5542.5]]
 
     snr = []
     for interval in intervals:
