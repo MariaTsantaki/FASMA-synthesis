b1e66177d1eb71caedac7f71cecec23e22a147fe
MariaTsantaki <Maria.Tsantaki@astro.up.pt>
2019-10-13 00:37:40 +0200

    python3 clean

--- interpolation.py
+++ interpolation.py
@@ -5,7 +5,6 @@ import gzip
 from scipy.interpolate import griddata
 from utils import GetModels
 
-
 def read_model(fname):
     '''Read the model atmosphere
 
@@ -24,27 +23,9 @@ def read_model(fname):
     model = np.loadtxt(data[23:-2])
     return model
 
+def interpolator_kurucz(params, atmtype='kurucz95'):
+    '''Interpolation for Kurucz'''
 
-def interpolator(params, save=True, atmtype='marcs', result=None):
-    '''This is a new approach based on a scipy interpolator.
-    Resembles the original interpolator we used but with a change
-
-    Input
-    -----
-    params : list of length 3
-      Teff, logg, [Fe/H] desired.
-    save : bool
-      Wether the new atmosphere should be saved. Default is True.
-    atmtype : str
-      The atmosphere models being used. Default is MARCS.
-
-    Output
-    ------
-    newatm : ndarray
-      New interpolated atmosphere.
-    '''
-
-    params = list(params)
     m = GetModels(params[0], params[1], params[2], atmtype=atmtype)
     mdict = m.getmodels()
     params[0] = mdict['teff'][0]
@@ -54,7 +35,6 @@ def interpolator(params, save=True, atmtype='marcs', result=None):
     teff = mdict['teff']
     logg = mdict['logg']
     feh = mdict['feh']
-
     # Making the surrounding grid points
     gridpoints = []
     for temp in teff[1]:
@@ -62,11 +42,10 @@ def interpolator(params, save=True, atmtype='marcs', result=None):
             for metal in feh[1]:
                 gridpoints.append((temp, grav, metal))
     gridpoints = np.asarray(gridpoints)
-
     # Define the points to obtain at the end
     teff = teff[0]
     logg = logg[0]
-    feh = feh[0]
+    feh  = feh[0]
     options = {'method': 'linear', 'rescale': True}
 
     # Reading the models
@@ -84,13 +63,105 @@ def interpolator(params, save=True, atmtype='marcs', result=None):
             newatm[layer, column] = griddata(gridpoints, tlayer, (teff, logg, feh), **options)
     vt_array = np.zeros(len(layers))+params[-1]*1e5
     newatm = np.hstack((newatm, vt_array[:, np.newaxis]))
+    return newatm
+
+def interpolator_marcs(params, fesun=7.47, microlim=3.0):
+    '''Interpolation for marcs. The function is taken from STEPAR
+    (Tabernero et al. 2019) to deal the gaps in the grid.'''
+
+    import _pickle as pic
+
+    gridMODS = open("/home/mtsantaki/Programs/models/marcs/MARCS1M.bin","rb")
+    tmod     = pic.load(gridMODS)
+    gmod     = pic.load(gridMODS)
+    mmod     = pic.load(gridMODS)
+    ltaumod  = pic.load(gridMODS)
+    Temod    = pic.load(gridMODS)
+    lpgmod   = pic.load(gridMODS)
+    lpemod   = pic.load(gridMODS)
+    rhoxmod  = pic.load(gridMODS)
+    kmod     = pic.load(gridMODS)
+    gridMODS.close()
+
+    x = list(params)
+    Teff  = np.round(x[0],0)
+    logg  = np.round(x[1],2)
+    metal = np.round(x[2],2)
+    micro = np.round(x[3],2)
+    sel = np.where((np.abs(tmod-Teff) <= 251.) & (np.abs(mmod-metal) <= 0.251) & (np.abs(gmod-logg) <= 0.5))
+    lT = np.shape(tmod[sel])[0]
+    lm = np.shape(mmod[sel])[0]
+    lg = np.shape(gmod[sel])[0]
+    if micro > microlim:
+        micro = microlim
+    if lT > 1 and lm > 1 and lg > 1 and micro <= microlim and micro >= 0.:
+        dT = max(tmod[sel])-min(tmod[sel])
+        dm = max(mmod[sel])-min(mmod[sel])
+        dg = max(gmod[sel])-min(gmod[sel])
+        testt = (min(tmod[sel]) <= Teff) and (max(tmod[sel]) >= Teff) and dT > 0.
+        testm = (min(mmod[sel]) <= metal) and (max(mmod[sel]) >= metal) and dm > 0.
+        testg = (min(gmod[sel]) <= logg) and (max(gmod[sel]) >= logg) and dg > 0.
+        if testt and testm and testg:
+            testint = True
+        else:
+            testint = False
+    else:
+        testint = False
+    if testint:
+        teint = griddata((tmod[sel], gmod[sel], mmod[sel]), Temod[sel], (Teff, logg, metal), method='linear', fill_value=np.nan, rescale=True)
+        if np.isfinite(teint[0]):
+            lpgint  = griddata((tmod[sel], gmod[sel], mmod[sel]), lpgmod[sel], (Teff, logg, metal),  method='linear', fill_value=np.nan, rescale=True)
+            lpeint  = griddata((tmod[sel], gmod[sel], mmod[sel]), lpemod[sel], (Teff, logg, metal),  method='linear', fill_value=np.nan, rescale=True)
+            rhoxint = griddata((tmod[sel], gmod[sel], mmod[sel]), rhoxmod[sel], (Teff, logg, metal), method='linear', fill_value=np.nan, rescale=True)
+            kint    = griddata((tmod[sel], gmod[sel], mmod[sel]), kmod[sel], (Teff, logg, metal),    method='linear', fill_value=np.nan, rescale=True)
+            nlayers = len(kint)
+            newatm = []
+            for i in range(nlayers):
+                newatm.append([rhoxint[i], teint[i], 10.**lpgint[i], 10.**lpeint[i], kint[i]])
+            newatm = np.array(newatm)
+            vt_array = np.zeros(nlayers)+params[-1]*1e5
+            newatm = np.hstack((newatm, vt_array[:, np.newaxis]))
+            return newatm
+        else:
+            return False
+    else:
+        return False
+
+def interpolator(params, save=True, atmtype='kurucz95', result=None):
+    '''This is a new approach based on a scipy interpolator.
+    Re1sembles the original interpolator we used but with a change
+
+    Input
+    -----
+    params : list of length 3
+      Teff, logg, [Fe/H] desired.
+    save : bool
+      Wether the new atmosphere should be saved. Default is True.
+    atmtype : str
+      The atmosphere models being used. Default is Kurucz95.
+
+    Output
+    ------
+    newatm : ndarray
+      New interpolated atmosphere.
+    '''
+
+    params = list(params)
+    if atmtype == 'marcs':
+        newatm = interpolator_marcs(params, fesun=7.47, microlim=3.0)
+    elif atmtype == 'kurucz95':
+        newatm = interpolator_kurucz(params, atmtype=atmtype)
+    elif atmtype == 'apogee_kurucz':
+        newatm = interpolator_kurucz(params, atmtype=atmtype)
+    else:
+        raise NameError('Could not find %s models' % atmtype)
+
     if save:
-        save_model(newatm, params, atmtype=atmtype)
+            save_model(newatm, params, type=atmtype)
     if result:
         return newatm, params
 
-
-def save_model(model, params, atmtype='marcs', fout='out.atm'):
+def save_model(model, params, type='kurucz95', fout='out.atm'):
     '''Save the model atmosphere in the right format
 
     Input
@@ -99,8 +170,8 @@ def save_model(model, params, atmtype='marcs', fout='out.atm'):
       The interpolated model atmosphere.
     params : list
       Teff, logg, [Fe/H], vt of the interpolated atmosphere.
-    atmtype : str
-      Type of atmospheric parameters. Default is MARCS
+    type : str
+      Type of atmospheric parameters. Default is Kurucz95
     fout : str
       Name of the saved atmosphere. Default is out.atm
 
@@ -109,16 +180,16 @@ def save_model(model, params, atmtype='marcs', fout='out.atm'):
     Atmospheric model.
     '''
     teff, logg, feh, vt = params
-    if atmtype in ['apogee_kurucz', 'marcs']:
+    if type in ['kurucz95', 'apogee_kurucz', 'marcs']:
         header = 'KURUCZ\n'\
                  'Teff= %i   log g= %.2f\n'\
                  'NTAU        %i' % (teff, logg, model.shape[0])
     else:
-        raise NameError('Could not find %s models' % atmtype)
+        raise NameError('Could not find %s models' % type)
 
     footer = '    %.3e\n'\
              'NATOMS     1  %.2f\n'\
-             '      26.0   %.2f\n'\
+             '      26.0    %.2f\n'\
              'NMOL      19\n'\
              '      606.0    106.0    607.0    608.0    107.0    108.0    112.0  707.0\n'\
              '       708.0    808.0     12.1  60808.0  10108.0    101.0     6.1    7.1\n'\
@@ -129,22 +200,18 @@ def save_model(model, params, atmtype='marcs', fout='out.atm'):
         model = np.column_stack((model, np.zeros_like(model[:, 0])))
     np.savetxt(fout, model, header=header, footer=footer, comments='', delimiter=' ', fmt=_fmt)
 
-
 if __name__ == '__main__':
     import argparse
     args = argparse.ArgumentParser(description='Get a model atmosphere.')
-    args.add_argument('teff', type=int, help='Effective temperature')
-    args.add_argument('logg', type=float, help='Surface gravity')
-    args.add_argument('feh', type=float, help='Metallicity, [Fe/H]')
-    args.add_argument('vt', type=float, help='Microturbulence')
-    args.add_argument('-o', '--out', help='Output atmosphere', default='out.atm')
-    args.add_argument('-a', '--atmosphere', help='Model atmosphere', choices=['kurucz08', 'apogee_kurucz', 'marcs'], default='kurucz08')
-
+    args.add_argument('teff', type=int,       help='Effective temperature')
+    args.add_argument('logg', type=float,     help='Surface gravity')
+    args.add_argument('feh',  type=float,     help='Metallicity, [Fe/H]')
+    args.add_argument('vt',   type=float,     help='Microturbulence')
+    args.add_argument('-o',   '--out',        help='Output atmosphere', default='out.atm')
+    args.add_argument('-a',   '--atmosphere', help='Model atmosphere', choices=['kurucz95', 'apogee_kurucz', 'marcs'], default='kurucz95')
     args = args.parse_args()
 
     params = [args.teff, args.logg, args.feh, args.vt]
-
     atmosphere, p = interpolator(params, save=False, atmtype=args.atmosphere, result=True)
-    save_model(atmosphere, params, atmtype=args.atmosphere, fout=args.out)
-
-    print 'Atmosphere model sucessfully saved in: %s' % args.out
+    save_model(atmosphere, params, type=args.atmosphere, fout=args.out)
+    print('Atmosphere model sucessfully saved in: %s' % args.out)
