6b6843247c27a6ba1b025da12f08dad2f7853de0
Maria Tsantaki <Maria.Tsantaki@astro.up.pt>
2024-11-15 12:12:13 +0100

    Update interpolation.py
    problem: model not found

--- FASMA/interpolation.py
+++ FASMA/interpolation.py
@@ -7,7 +7,6 @@ import gzip
 from .solar_abundance import solar
 from .utils import GetModels
 
-
 def read_model(fname):
     '''Read the KURUCZ model atmosphere.
 
@@ -26,7 +25,6 @@ def read_model(fname):
     data = f.readlines()
     model = np.loadtxt(data[23:-2])
     return model
-
 
 def solar_abundance(element):
     '''For a given atomic number, return solar abundance from Asplund et al. 2009.
@@ -48,7 +46,6 @@ def solar_abundance(element):
         return None, None
     index = elements.symbol(element).number
     return index, abundance
-
 
 def interpolator_kurucz(params, atmtype='apogee_kurucz'):
     '''Interpolation for Kurucz model atmospheres.
@@ -107,8 +104,64 @@ def interpolator_kurucz(params, atmtype='apogee_kurucz'):
     newatm = np.hstack((newatm, vt_array[:, np.newaxis]))
     return newatm
 
+def interpolator_marcs(params, atmtype='marcs'):
+    '''Interpolation for Kurucz model atmospheres.
+    Input
+    -----
+    params : ndarray
+      Stellar parameters for the Interpolation.
 
-def interpolator_marcs(params, fesun=7.47, microlim=3.0):
+    Output
+    ------
+    newatm : ndarray
+      The interpolated atmosphere, the columns and tauross in a tuple
+    '''
+
+    m = GetModels(params[0], params[1], params[2], atmtype=atmtype)
+    mdict = m.getmodels()
+    if mdict is False:
+        return False
+
+    params[0] = mdict['teff'][0]
+    params[1] = mdict['logg'][0]
+    params[2] = mdict['feh'][0]
+    mnames = mdict['models']
+    teff = mdict['teff']
+    logg = mdict['logg']
+    feh = mdict['feh']
+    # Making the surrounding grid points
+    gridpoints = []
+    for temp in teff[1]:
+        for grav in logg[1]:
+            for metal in feh[1]:
+                gridpoints.append((temp, grav, metal))
+    gridpoints = np.asarray(gridpoints)
+    # Define the points to obtain at the end
+    teff = teff[0]
+    logg = logg[0]
+    feh = feh[0]
+    options = {'method': 'linear', 'rescale': True}
+
+    # Reading the models
+    models = []
+    for mname in mnames:
+        tatm = read_model(mname)
+        models.append(tatm)
+
+    layers = range(min([model.shape[0] for model in models]))
+    columns = range(6)
+    newatm = np.zeros((len(layers), len(columns)))
+    for layer in layers:
+        for column in columns:
+            tlayer = [model[layer, column] for model in models]
+            newatm[layer, column] = griddata(
+                gridpoints, tlayer, (teff, logg, feh), **options
+            )
+    vt_array = np.zeros(len(layers)) + params[-1] * 1e5
+    newatm = np.hstack((newatm, vt_array[:, np.newaxis]))
+    return newatm
+
+def interpolator_marcs_(params, fesun=7.41, microlim=3.0):
     '''Interpolation for marcs models. The function is taken from STEPAR
     (Tabernero et al. 2019) to deal the gaps in the grid.
     Input
@@ -124,6 +177,7 @@ def interpolator_marcs(params, fesun=7.47, microlim=3.0):
 
     import _pickle as pic
     import os
+
     cwd = os.path.dirname(os.path.abspath(__file__))
     fname = cwd + '/models/marcs/MARCS1M.bin'
     if not os.path.isfile(fname):
@@ -141,27 +195,24 @@ def interpolator_marcs(params, fesun=7.47, microlim=3.0):
         kmod = pic.load(gridMODS)
 
     x = list(params)
-    Teff = np.round(x[0], 0)
+    Teff = int(x[0])
     logg = np.round(x[1], 2)
     metal = np.round(x[2], 2)
     micro = np.round(x[3], 2)
-    sel = np.where(
-        (np.abs(tmod - Teff) <= 251.0)
-        & (np.abs(mmod - metal) <= 0.251)
-        & (np.abs(gmod - logg) <= 0.5)
-    )
+    sel = np.where((np.abs(tmod - Teff) <= 251.0) & (np.abs(mmod - metal) <= 0.251) & (np.abs(gmod - logg) <= 0.5))
     lT = np.shape(tmod[sel])[0]
     lm = np.shape(mmod[sel])[0]
     lg = np.shape(gmod[sel])[0]
     if micro > microlim:
         micro = microlim
+
     if lT > 1 and lm > 1 and lg > 1 and micro <= microlim and micro >= 0.0:
         dT = max(tmod[sel]) - min(tmod[sel])
-        dm = max(mmod[sel]) - min(mmod[sel])
         dg = max(gmod[sel]) - min(gmod[sel])
+        dm = max(mmod[sel]) - min(mmod[sel])
         testt = (min(tmod[sel]) <= Teff) and (max(tmod[sel]) >= Teff) and dT > 0.0
-        testm = (min(mmod[sel]) <= metal) and (max(mmod[sel]) >= metal) and dm > 0.0
         testg = (min(gmod[sel]) <= logg) and (max(gmod[sel]) >= logg) and dg > 0.0
+        testm = (min(mmod[sel]) <= metal) and (max(mmod[sel]) >= metal) and dm > 0.0
         if testt and testm and testg:
             testint = True
         else:
@@ -169,72 +220,28 @@ def interpolator_marcs(params, fesun=7.47, microlim=3.0):
     else:
         testint = False
     if testint:
-        teint = griddata(
-            (tmod[sel], gmod[sel], mmod[sel]),
-            Temod[sel],
-            (Teff, logg, metal),
-            method='linear',
-            fill_value=np.nan,
-            rescale=True,
-        )
+
+        teint = griddata((tmod[sel], gmod[sel], mmod[sel]), Temod[sel], (Teff, logg, metal), method='linear', fill_value=np.nan, rescale=True)
         if np.isfinite(teint[0]):
-            lpgint = griddata(
-                (tmod[sel], gmod[sel], mmod[sel]),
-                lpgmod[sel],
-                (Teff, logg, metal),
-                method='linear',
-                fill_value=np.nan,
-                rescale=True,
-            )
-            lpeint = griddata(
-                (tmod[sel], gmod[sel], mmod[sel]),
-                lpemod[sel],
-                (Teff, logg, metal),
-                method='linear',
-                fill_value=np.nan,
-                rescale=True,
-            )
-            rhoxint = griddata(
-                (tmod[sel], gmod[sel], mmod[sel]),
-                rhoxmod[sel],
-                (Teff, logg, metal),
-                method='linear',
-                fill_value=np.nan,
-                rescale=True,
-            )
-            kint = griddata(
-                (tmod[sel], gmod[sel], mmod[sel]),
-                kmod[sel],
-                (Teff, logg, metal),
-                method='linear',
-                fill_value=np.nan,
-                rescale=True,
-            )
+            lpgint = griddata((tmod[sel], gmod[sel], mmod[sel]), lpgmod[sel], (Teff, logg, metal), method='linear', fill_value=np.nan, rescale=True)
+            lpeint = griddata((tmod[sel], gmod[sel], mmod[sel]), lpemod[sel], (Teff, logg, metal), method='linear', fill_value=np.nan, rescale=True)
+            rhoxint = griddata((tmod[sel], gmod[sel], mmod[sel]), rhoxmod[sel], (Teff, logg, metal), method='linear', fill_value=np.nan, rescale=True)
+            kint = griddata((tmod[sel], gmod[sel], mmod[sel]), kmod[sel], (Teff, logg, metal), method='linear', fill_value=np.nan, rescale=True)
             nlayers = len(kint)
             newatm = []
             for i in range(nlayers):
-                newatm.append(
-                    [
-                        rhoxint[i],
-                        teint[i],
-                        10.0 ** lpgint[i],
-                        10.0 ** lpeint[i],
-                        kint[i],
-                    ]
-                )
+                newatm.append([rhoxint[i], teint[i], 10.0 ** lpgint[i], 10.0 ** lpeint[i], kint[i], ])
             newatm = np.array(newatm)
             vt_array = np.zeros(nlayers) + params[-1] * 1e5
             newatm = np.hstack((newatm, vt_array[:, np.newaxis]))
             return newatm
         else:
+            print('There is a problem in the interpolation')
             return False
     else:
         return False
 
-
-def interpolator(
-    params, abund=0.0, elem=False, save=True, atmtype='apogee_kurucz', result=None
-):
+def interpolator(params, abund=0.0, elem=False, save=True, atmtype='apogee_kurucz', result=None):
     '''Function to connect all. For a given set of params, return a model atmosphere.
 
     Input
@@ -260,19 +267,19 @@ def interpolator(
 
     params = list(params)
     if atmtype == 'marcs':
-        newatm = interpolator_marcs(params, fesun=7.47, microlim=3.0)
+        # newatm = interpolator_marcs(params, fesun=7.41, microlim=3.0)
+        newatm = interpolator_marcs(params, atmtype=atmtype)
     elif atmtype == 'apogee_kurucz':
         newatm = interpolator_kurucz(params, atmtype=atmtype)
     else:
-        raise NameError('Could not find %s models', atmtype)
+        raise NameError('Could not find model atmosphere: ', atmtype)
 
     if newatm is False:
-        raise NameError('Could not find %s models', atmtype)
+        raise NameError('Could not find model atmosphere: ', atmtype)
     if save:
         save_model(newatm, params, abund=abund, elem=elem, type=atmtype)
     if result:
         return newatm, params
-
 
 def save_model(model, params, abund=0.0, elem=False, type='apogee_kurucz', fout='out.atm'):
     '''Save the model atmosphere in the right format.
@@ -318,7 +325,7 @@ def save_model(model, params, abund=0.0, elem=False, type='apogee_kurucz', fout=
             '      606.0    106.0    607.0    608.0    107.0    108.0    112.0  707.0\n'
             '       708.0    808.0     12.1  60808.0  10108.0    101.0     6.1    7.1\n'
             '         8.1    822.0     22.1'
-            % (vt * 1e5, feh, 7.47 + feh, num, solabund + abund)
+            % (vt * 1e5, feh, 7.41 + feh, num, solabund + abund)
         )
 
     else:
@@ -329,7 +336,7 @@ def save_model(model, params, abund=0.0, elem=False, type='apogee_kurucz', fout=
             'NMOL      19\n'
             '      606.0    106.0    607.0    608.0    107.0    108.0    112.0  707.0\n'
             '       708.0    808.0     12.1  60808.0  10108.0    101.0     6.1    7.1\n'
-            '         8.1    822.0     22.1' % (vt * 1e5, feh, 7.47 + feh)
+            '         8.1    822.0     22.1' % (vt * 1e5, feh, 7.41 + feh)
         )
 
     _fmt = ('%15.8E', '%8.1f', '%.3E', '%.3E', '%.3E', '%.3E', '%.3E')
